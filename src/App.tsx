36050f751bee3a7dbd638573b85a7640bbfdba2a
Kamal Dev <kamaldev.bhuyan@gmail.com>
2024-06-27 08:29:06 +0530

    feat: add upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import { ListBox, StackLayout, Option, Text, Spinner, FlexLayout, NavigationItem } from '@salt-ds/core';
+import { ListBox, StackLayout, Option, Text, Spinner, FlexLayout, StatusIndicator, Button, Drawer, DrawerCloseButton, H2, FileDropZone, FileDropZoneIcon, FileDropZoneTrigger } from '@salt-ds/core';
 import './App.scss'
+import { AddDocumentIcon, AddIcon } from '@salt-ds/icons';
+import { SyntheticEvent, useId, useState } from 'react';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const id = useId();
+
+  const handleRequestOpen = () => {
+    setOpen(true);
+  };
+
+  const onOpenChange = (newOpen: boolean) => {
+    setOpen(newOpen);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const validate = (event: SyntheticEvent, files: File[]) => {
+    console.log("validate files", files);
+  };
+  
   return (
     <>
       <div className='app-container'>
         <nav>
-          <FlexLayout justify='space-between' gap={3}>
+          <FlexLayout justify='space-between' gap={3} align='center'>
             <h1 className='logo'>Meeting Assist</h1>
-            <NavigationItem>
 
-            </NavigationItem>
+            <div>
+              <Button variant="primary" onClick={handleRequestOpen}>
+                <AddIcon/> Upload
+              </Button>
+            </div>
           </FlexLayout>
         </nav>
         <div className='side-bar'>
@@ -33,7 +57,7 @@ function App() {
                   <strong>{'Blue'}</strong>
                 </Text>
                 <Text styleAs="label" color="secondary">
-                  {'#cc0000'}
+                  {'#cc0000'} <small><StatusIndicator status="warning" /> </small>
                 </Text>
               </StackLayout>
             </Option>
@@ -59,6 +83,31 @@ function App() {
           </StackLayout>
         </main>
       </div>
+
+      <Drawer
+        open={open}
+        onOpenChange={onOpenChange}
+        position="right"
+        style={{ height: '100vh' }}
+        aria-labelledby={id}
+      >
+        <DrawerCloseButton onClick={handleClose} />
+        <H2 id={id} style={{ paddingBottom: "var(--salt-spacing-300)" }}>
+          Marginal Tiering
+        </H2>
+        <div style={{ height: "calc(3 * var(--salt-size-base))" }}
+        >
+          <FileDropZone
+            style={{ width: 300 }}
+            onDrop={(event, files) => validate(event, files)}
+          >
+            <FileDropZoneIcon />
+            <strong>Drop files here or</strong>
+            <FileDropZoneTrigger accept=".png" onChange={validate} />
+            <Text>Only .txt, .mp3 and .mp4 files</Text>
+          </FileDropZone>
+        </div>
+      </Drawer>
     </>
   )
 }
